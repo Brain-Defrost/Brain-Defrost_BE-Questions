60ba0faaf21e205ef9f9900eb223f9d9306554a1
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-06-01 16:15:29 -0600

    Fix test to enhace error handling

--- spec/requests/api/v1/questions_spec.rb
+++ spec/requests/api/v1/questions_spec.rb
@@ -1,77 +1,57 @@
-require 'swagger_helper'
+require 'rails_helper'
 
-RSpec.describe 'api/v1/questions', type: :request do
+RSpec.describe QuestionService do
+  describe '.create_trivia_questions(topic, question_number)' do
+    let!(:question_number) { 3 }
+    let!(:topic) { "music" }
 
-  path '/api/v1/questions' do
+    context 'happy path' do
+      it 'returns generated trivia questions', :vcr do
+        service_response = QuestionService.create_trivia_questions(topic, question_number)
 
-    post('Create a list trivia questions') do
-      tags 'Questions'
-      consumes 'application/json'
-      produces 'application/json'
+        expect(service_response).to be_a(Hash)
+        check_hash_structure(service_response, :data, Array)
 
-      parameter name: :params, in: :body, required: true, schema: {
-        type: :object,
-        properties: {
-          topic: { type: :string, required: true },
-          number_of_questions: { type: :integer, required: true }
-        }
-      }
-
-      response(201, 'successful') do
-        let(:params) { {
-          topic: "sorcery",
-          number_of_questions: 8,
-        }}
-
-        schema({
-          type: :object,
-          properties: {
-            data: {
-              type: :array,
-              items: {
-                type: :object,
-                properties: {
-                  id: { type: [:string, :null] },
-                  type: { type: :string, required: true },
-                  attributes: {
-                    type: :object,
-                    properties: {
-                      question_text: { type: :string },
-                      question_number: { type: :string },
-                      answer: { type: :string },
-                      options: {
-                        type: :array,
-                        items: { type: :string }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        })
-
-        run_test! vcr: true do |example|
-          expect(response).to have_http_status(201)
-
-          parsed_data = JSON.parse(response.body, symbolize_names: true)
-          expect(parsed_data[:data].size).to eq 8
-        end
-      end
-
-      response(500, 'Server Error - Cannot create questions') do
-        let(:params) { {
-          topic: "sorcery",
-          number_of_questions: 8,
-        }}
-
-        run_test! vcr: true do |example|
-          expect(response).to have_http_status(500)
-
-          error = JSON.parse(response.body, symbolize_names: true)[:error]
-          expect(error[:message]).to eq "Unable to create trivia questions at this time"
+        service_response[:data].each do |data|
+          check_hash_structure(data, :id, String)
+          check_hash_structure(data, :attributes, Hash)
+          check_hash_structure(data[:attributes], :question_text, String)
+          check_hash_structure(data[:attributes], :correct_answer, String)
+          check_hash_structure(data[:attributes], :answer_options, Array)
+          expect(data[:attributes][:answer_options].size).to eq(4)
+          expect(data[:attributes][:answer_options]).to all(be_a(String))
         end
       end
     end
+
+    context 'sad path' do
+      it 'returns error message if request is unsuccessful' do
+        stub_request(:post, 'https://api.openai.com/v1/chat/completions')
+          .to_return(status: 500, body: '', headers: {})
+
+        service_response = QuestionService.create_trivia_questions(topic, question_number)
+        expect(service_response).to eq('Unable to create trivia questions at this time')
+      end
+
+      it 'returns error message if response body cannot be parsed' do
+        stub_request(:post, 'https://api.openai.com/v1/chat/completions')
+          .to_return(status: 200, body: 'invalid json', headers: {})
+
+        service_response = QuestionService.create_trivia_questions(topic, question_number)
+        expect(service_response).to eq("Unable to parse trivia questions at this time")
+      end
+
+      it 'returns error message if there is a connection error' do
+        allow(QuestionService).to receive(:post_url).and_raise(Faraday::ConnectionFailed.new('Connection failed'))
+
+        service_response = QuestionService.create_trivia_questions(topic, question_number)
+        expect(service_response).to eq("Unable to connect to the trivia service at this time")
+      end
+    end
+  end
+
+  def check_hash_structure(object, key, klass)
+    expect(object).to have_key(key)
+    expect(object[key]).to be_a(klass)
   end
 end
