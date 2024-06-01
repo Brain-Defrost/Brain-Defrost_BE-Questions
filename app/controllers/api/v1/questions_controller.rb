1070a2d8dd0678e32650c509ec03abef5c4e7d8a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-06-01 16:14:59 -0600

    Fix application controller for error handling

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::QuestionsController < ApplicationController
   def create
     questions = QuestionFacade.create_questions(params[:topic], params[:number_of_questions])
-    if questions.class == String
-      render json: { error: { message: questions} }, status: :internal_server_error
+    if questions.is_a?(String)
+      handle_service_error(questions)
     else
       render json: QuestionSerializer.new(questions), status: :created
     end
+  rescue StandardError => e
+    handle_internal_server_error(e)
   end
 end
